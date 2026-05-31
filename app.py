5f03602c6be336a70eeade4c59daa3978c58fc1f
Prathmesh Patil <prathmeshpatil.it@gmail.com>
2026-05-31 07:06:44 -1200

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
+pdf_path = "data/sample.pdf"
+
+loader = PyPDFLoader(pdf_path)
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.from_documents(chunks, embeddings)
+
+db.save_local("vectorstore")
+print("Vector database created successfully")
